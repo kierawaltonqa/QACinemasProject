3b8501b1562d7bcf9524686ba46491fb4e7670ec
Kiera Walton <kwalton@qa.com>
2021-03-01 11:15:42 +0000

    navbar implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
 import './Resources/App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navigation from './Components/Navigation';
 
 function App() {
   return (
-    <div className="App">
+    <div className="page-container">
+      <Router>
+        <Navigation />
+        <Switch>
+          <Route path="/" exact>
+
+          </Route>
+        </Switch>
+      </Router>
+
 
     </div>
   );
---
+++ frontend/src/Components/Navigation.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
+} from 'reactstrap';
+import { Form, FormControl, Button } from 'react-bootstrap';
+
+
+const Navigation = (props) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+
+    return (
+        <div>
+            <Navbar color="light" light expand="md" id="navbar">
+                <NavbarToggler onClick={toggle} />
+                <Collapse isOpen={isOpen} navbar>
+                    <Nav className="mr-auto" navbar>
+                        <NavItem>
+                            <NavLink href='/' >Home</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href='/about' >About</NavLink>
+                        </NavItem>
+                        <UncontrolledDropdown nav inNavbar>
+                            <DropdownToggle nav caret>
+                                Movies
+                            </DropdownToggle>
+                            <DropdownMenu right>
+                                <DropdownItem>
+                                    Film Listings
+                                </DropdownItem>
+                                <DropdownItem>
+                                    New Releases
+                                </DropdownItem>
+                                <DropdownItem>
+                                    Film Classification
+                                </DropdownItem>
+                            </DropdownMenu>
+                        </UncontrolledDropdown>
+                        <UncontrolledDropdown nav inNavbar>
+                            <DropdownToggle nav caret>
+                                Bookings
+                            </DropdownToggle>
+                            <DropdownMenu right>
+                                <DropdownItem>
+                                    Screens
+                                </DropdownItem>
+                                <DropdownItem>
+                                    Tickets
+                                </DropdownItem>
+                                <DropdownItem>
+                                    Payment
+                                </DropdownItem>
+                            </DropdownMenu>
+                        </UncontrolledDropdown>
+                        <NavItem>
+                            <NavLink href='/discussion' >Discussion</NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+                <Form inline>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                    <Button variant="outline-success">Search</Button>
+                </Form>
+            </Navbar>
+        </div>
+    )
+}
+export default Navigation;
