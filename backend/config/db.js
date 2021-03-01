41acf39ddda2354c2e3a2740268c8504879e056f
Kiera Walton <kwalton@qa.com>
2021-03-01 10:29:00 +0000

    changed url - kiera

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,8 @@
 const { MongoClient } = require("mongodb");
 
 // Replace the following with your Atlas connection string                                                                                                                                        
-const url = "mongodb+srv://cinema:root@spellcluster.tnmib.mongodb.net/test?authSource=admin&replicaSet=atlas-nzfvt5-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true";
+const url = "mongodb+srv://cinema:root@spellcluster.tnmib.mongodb.net/test";
+
 const client = new MongoClient(url);
 async function run() {
     try {
