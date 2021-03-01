5ab0a6ab9dd6ed62672e3b02f573e647c56f20be
Kiera Walton <kwalton@qa.com>
2021-03-01 14:20:28 +0000

    added images and header to listings page

--- frontend/src/Components/Movies/Listings.jsx
+++ frontend/src/Components/Movies/Listings.jsx
@@ -4,6 +4,12 @@ import ListingsFilm from './ListingsFilm';
 const Listings = () => {
     return (
         <div className="container">
+            <div className="row-md" id="listingheader">
+                <hr />
+                <h3>Movies Available To Watch.</h3>
+                <hr />
+                <p>On this page you will find a summary of all of the films we have available for viewing at QA Cinemas. Select the details button under each film to view more about it.</p>
+            </div>
             <div className="row">
                 {
                     data.map((film) => (
