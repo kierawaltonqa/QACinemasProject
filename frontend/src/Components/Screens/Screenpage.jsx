0934a9811e7fc3694c84a25914ef232019b87b9a
RobTolley <rtolley@qa.com>
2021-03-02 12:22:59 +0000

    ADD: basic buttons

--- frontend/src/Components/Screens/Screenpage.jsx
+++ frontend/src/Components/Screens/Screenpage.jsx
@@ -6,6 +6,7 @@ import "./Screen.css"
 import pic4 from "./ScreenPics/deluxeSeating1.png"
 import pic5 from "./ScreenPics/deluxeSeating2.png"
 import pic6 from "./ScreenPics/DeluxeSeatingPlan.png"
+import ScreensReadMore from "./ScreensReadMore";
 
 
 
@@ -17,23 +18,44 @@ const Screenpage = () => {
     return (
 
 
+        // Carousel
+
         <>
-        <br/>
-        <div className="container">
-        <div className="row" >
-        <div className="col-6" >
-        <Carousel image1={pic2} image2={pic3} image3={pic1}/>
-        </div>
-        <div className="col-6">
-            <Carousel image1={pic4} image2={pic5} image3={pic6}/>
-        </div>
-        </div>
-        </div>
-        {/* <img src={StandardSeatingPlan} alt=""/> */}
+            <br />
+            <div className="row" style={{ height: "400px" }} >
+                <div className="slider-container ">
+                    <Carousel image1={pic2} image2={pic3} image3={pic1} />
+                </div>
+                <div className="slider-container">
+                    <Carousel image1={pic4} image2={pic5} image3={pic6} style={{ height: "400px" }} />
+                </div>
+            </div>
+            <br />
+
+            {/* Name of the seating plans  */}
+
+
+            <div className="row">
+                <div className="col-6">
+                    <p id="beauty">Standard Seating</p>
+                </div>
+                <div className="col-6">
+                    <p id="beauty" style={{ textAlign: "left" }}>Deluxe Seating</p>
+                </div>
+            </div>
+
+            {/* Buttons for Read More */}
+
+            <div className="row">
+                    <ScreensReadMore />
+                    <ScreensReadMore />
+            </div>
+
+
 
         </>
     )
-    
+
 }
 
 
---
+++ frontend/src/Components/Screens/ScreensReadMore.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ScreensReadMore = () => {
+    return (
+        <div className="col-6">
+
+            <button className="btn btn-secondary">Read More</button>
+            
+        </div>
+    )
+}
+
+export default ScreensReadMore
