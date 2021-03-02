046cc6203cdca16f87285028bf64ddd91d916e27
Kiera Walton <kwalton@qa.com>
2021-03-02 10:39:05 +0000

    keeping modal small and not full screen

--- frontend/src/Components/Movies/MovieListings/ListingsDetails.jsx
+++ frontend/src/Components/Movies/MovieListings/ListingsDetails.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { CardLink, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
+
 const ListingsDetails = ({ id, title, runtime, rating, director, poster, actors }) => {
 
     const [modal, setModal] = useState(false);
@@ -9,38 +10,41 @@ const ListingsDetails = ({ id, title, runtime, rating, director, poster, actors 
     return (
         <>
             <CardLink className="btn btn-outline-dark" onClick={toggle}>Details</CardLink>
-            <Modal className="modal-container custom-map-modal"
-                dialogClassName="modal-dialog"
-                isOpen={modal}>
-                <ModalHeader>{title}</ModalHeader>
-                <ModalBody>
-                    <div class="row-md">
-                        <div className="col-md-12" id="modalimage">
-                            <img src={poster} height="30%" width="30%" />
-                        </div>
-                        <div className="col-md-12">
-                            <hr />
-                            <p>Directed by: {director}</p>
-                            <p>The main cast:
+
+            <div className="modal-dialog modal-fullscreen-sm-down">
+                <Modal className="modal-container custom-map-modal"
+                    id="listingmodal"
+                    dialogClassName="modal-dialog"
+                    isOpen={modal}>
+                    <ModalHeader>{title}</ModalHeader>
+                    <ModalBody>
+                        <div class="row-md">
+                            <div className="col-md-12" id="modalimage">
+                                <img src={poster} height="30%" width="30%" />
+                            </div>
+                            <div className="col-md-12">
+                                <hr />
+                                <p>Directed by: {director}</p>
+                                <p>The main cast:
                                     <ul>
-                                    {
-                                        actors.map((actor) => (
-                                            <li>{actor}</li>
-                                        ))
-                                    }
-                                </ul>
-                            </p>
-                            <p>Runtime: {runtime}</p>
-                            <p>Rating: {rating}</p>
+                                        {
+                                            actors.map((actor) => (
+                                                <li>{actor}</li>
+                                            ))
+                                        }
+                                    </ul>
+                                </p>
+                                <p>Runtime: {runtime}</p>
+                                <p>Rating: {rating}</p>
+                            </div>
                         </div>
-                    </div>
 
-                </ModalBody>
-                <ModalFooter id="modalfooter">
-                    <button onClick={toggle} className="btn btn-outline-danger">Go Back</button>
-                </ModalFooter>
-            </Modal>
-
+                    </ModalBody>
+                    <ModalFooter id="modalfooter">
+                        <button onClick={toggle} className="btn btn-outline-danger">Go Back</button>
+                    </ModalFooter>
+                </Modal>
+            </div>
         </>
     )
 }
