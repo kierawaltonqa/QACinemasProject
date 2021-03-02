99066f6a3de7dd360e1b40d4031d33fab7355c55
Mohamud Mussa <70893158+MohamudMussa@users.noreply.github.com>
2021-03-02 14:08:09 +0000

    Completed

--- frontend/src/Components/CantFindPage.jsx
+++ frontend/src/Components/CantFindPage.jsx
@@ -1,47 +1,26 @@
-import React from 'react'
-import {Row, Col, Container, Image} from 'react-bootstrap/'
+import React from "react";
+import { Row, Col, Container, Image } from "react-bootstrap/";
 import "../Css/AboutStyle.css";
-import lost from '../Resources/Logo/lost.jpg'
-import Footer from './Footer'
+import lost from "../Resources/Logo/lost.jpg";
+import Footer from "./Footer";
 
 const CantFindPage = () => {
-    return (
-        <> 
-        <Container maxWidth="lg">
-        
-             <Container>
-               <div className="row">
-                 <Col>
-            
-                   
-                 </Col>
-                 <Col sm={6}>
-                 
-                 </Col>
-               </div>
-                
-        
-               </Container>
-                </Container>
+  return (
+    <>
+      <div className="row">
+        <Col sm={6}>
+          <Image className="img" src={lost} roundedCircle />
+        </Col>
+        <Col>
+          <Col>
+            <h1 className="mid"> Ooops, you seem lost!</h1>
+          </Col>
+        </Col>
+      </div>
 
-                <div className="row">
-            <Col sm={6}>
-            <Image className='img' src={lost} roundedCircle />
-            </Col>
-            <Col>
-              <Col>
+      <Footer></Footer>
+    </>
+  );
+};
 
-              <h1 className="mid"> Ooops, you seem lost!</h1>
-
-
-              </Col>
-            </Col>
-          </div>
-
-                <Footer></Footer>
-        </>
-
-    )
-}
-
-export default CantFindPage
+export default CantFindPage;
