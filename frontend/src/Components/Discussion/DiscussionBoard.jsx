2f321ddebc0ce7e1d17fdcb569fcfbda2e87b92e
Kiera Walton <kwalton@qa.com>
2021-03-03 10:00:03 +0000

    added image to discussion board

--- frontend/src/Components/Discussion/DiscussionBoard.jsx
+++ frontend/src/Components/Discussion/DiscussionBoard.jsx
@@ -2,6 +2,7 @@ import PostDiscussion from "./PostDiscussion";
 import { useState } from 'react';
 import ReadDiscussion from "./ReadDiscussion";
 import './Discussion.css';
+import penguin from '../../Resources/penguin2.jpg'
 
 const DiscussionBoard = () => {
 
@@ -13,12 +14,19 @@ const DiscussionBoard = () => {
 
     return (
         <div className="container" >
-            <div className="row-md">
-                <hr />
-                <h2 id="discussionboard-title">Discussion Board.</h2>
-                <hr />
-                <h4 id="discussionboard-subtitle">Post here to discuss and rate the films you have watched...</h4>
-                <br />
+            <div className="row">
+                <div className="col-md-3">
+                    <br />
+                    <img src={penguin} alt="penguin" id="penguin" />
+                </div>
+                <div className="col-md-9">
+                    <hr />
+                    <h2 id="discussionboard-title">Discussion Board.</h2>
+                    <hr />
+
+                    <h4 id="discussionboard-subtitle">Post here to discuss and rate the films you have watched...</h4>
+                    <br />
+                </div>
             </div>
             <div className="row">
                 <div className="col-md-3">
