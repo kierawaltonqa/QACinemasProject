cbf34e0631e715ecabd11849a9748a5960a8524d
Kiera Walton <kwalton@qa.com>
2021-03-03 12:47:53 +0000

    started writing intro for welcome section

--- frontend/src/Components/HomePage/Home.jsx
+++ frontend/src/Components/HomePage/Home.jsx
@@ -4,25 +4,37 @@ import CarouselPage from './Carousel';
 import image1 from '../../Resources/cinema1.jpg';
 import image2 from '../../Resources/cinema4.jpg';
 import image3 from '../../Resources/cinema5.jpg';
+import WelcomePage from './WelcomePage';
 
 const Home = () => {
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md-12">
-                    <br />
-                    <h1 id="title">Welcome To QA Cinemas
-                        <img src={penguin} alt="penguin-wave" id="penguin-wave" />
-                    </h1>
+        <>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <br />
+                        <br />
+                        <h1 id="title">Welcome To QA Cinemas
+                        {/* <img src={penguin} alt="penguin-wave" id="penguin-wave" /> */}
+                        </h1>
+                        <br />
+                    </div>
+                </div>
+                <div className="row" style={{ height: "500px" }}>
+                    <div className="col-md-12 slider-container" id="carousel">
+                        <CarouselPage image1={image1} image2={image2} image3={image3} />
+                    </div>
+                </div>
+                <div className="row">
+                    <WelcomePage />
+                </div>
+            </div>
 
-                </div>
-            </div>
-            <div className="row-md">
-                <div className="col-md-12 slider-container">
-                    <CarouselPage image1={image1} image2={image2} image3={image3} />
-                </div>
-            </div>
-        </div>
+
+
+
+
+        </>
     )
 }
 export default Home;
---
+++ frontend/src/Components/HomePage/WelcomePage.jsx
@@ -0,0 +1,14 @@
+const WelcomePage = () => {
+    return (
+        <div id="welcomepage">
+            <h4>We are bringing the latest and best rated films to a screen near you.</h4>
+            <p>Browse this website to:</p>
+            <ul>
+                <li> view our current listings and new releases</li>
+                <li>book tickets for films and view on your preferred choice of screen </li>
+                <li>post on our discussion page to express your opinions and discuss the films you have watched</li>
+            </ul>
+        </div>
+    )
+}
+export default WelcomePage;
