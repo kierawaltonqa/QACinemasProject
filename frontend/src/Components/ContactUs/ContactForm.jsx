64918977060c5009390bd28a9647b59c4e203db2
Mohamud Mussa <70893158+MohamudMussa@users.noreply.github.com>
2021-03-03 14:15:23 +0000

    concept

--- frontend/src/Components/ContactUs/ContactForm.jsx
+++ frontend/src/Components/ContactUs/ContactForm.jsx
@@ -1,37 +1,78 @@
 import React from 'react'
+import {Form, Button} from 'react-bootstrap'
 import { useState } from 'react';
 
 const ContactForm = () => {
 
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-
-    const printValues = e => {
+    const printToScreen = (e) => {
         e.preventDefault();
-        console.log(username,password);
+
+        console.log({name, email,subject})
+   
     }
-    
-    return (
+
+    const sendEmail = (e) => {
+       e.preventDefault();
+       return window.location = mailto : "xyz@abc.com";
+       
+
+    }
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [subject, setSubject] = useState('')
+
+  
+
+     return(
         <>
-        <form onSubmit={printValues}>
-        <label> Username:</label>
-            <input name="username" 
-            type="text" 
-            value={username} 
-            onChange={e => setUsername(e.target.value)}/>
+        <di className="container" className="title">
+        <form>
 
-        <label> Password: </label>
-            <input type="password"
-            name="password"
-            value={password}
-            onChange={e => setPassword(e.target.value)}/>
-        <br/>
-        <button>Submit</button>
+        <label>Name</label>
+        <input 
+        type="text"
+        name="name"
+        value={name}
+       onChange={e => setName(e.target.value)} />       
+
+
+<br/>
+       
+<label>Email</label>
+        <input 
+        type="text"
+        email="email"
+        value={email}
+       onChange={e => setEmail(e.target.value)} />       
+
+
+<br/>
+<label>Subject</label>
+        <input 
+        type="text"
+        subject="subject"
+        value={subject}
+       onChange={e => setSubject(e.target.value)} />       
+
+
+
+
+<br/>
+
+{/* <a class="mailto" href="mailto:contact@test.com">Send</a> */}
+
+<button onclick={sendEmail}>Send e-mail</button>
+
+
+
+
+
         </form>
-
-        <h1> {printValues} </h1>
-    </>
-    );
+        
+        </di>
+        </>
+     );
 }
 
 export default ContactForm
