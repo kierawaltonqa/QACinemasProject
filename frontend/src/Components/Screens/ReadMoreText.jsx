bac6950b2dd5db07659bbbd45c9d918333dd8c25
scostello-hubQA <scostello@academytrainee.com>
2021-03-05 09:47:55 +0000

    SC: Strict equality endorsed on readmoretext

--- frontend/src/Components/Screens/ReadMoreText.jsx
+++ frontend/src/Components/Screens/ReadMoreText.jsx
@@ -1,30 +1,30 @@
 import React from 'react'
 import ScreenInfoCard from './ScreenInfoCard';
 
-const ReadMoreText = ({toggleOn}) => {
+const ReadMoreText = ({ toggleOn }) => {
 
 
-    if (toggleOn == "standard") {
+    if (toggleOn === "standard") {
         const title = "Standard Seating";
-        const cost = "£8.50   per ticket " ;
+        const cost = "£8.50   per ticket ";
         const desc = "Standard seating allows you access to the large array of seating inside our cinema! Providing you with the full QACinema Experience. ";
-        return(
-        
+        return (
+
             <ScreenInfoCard title={title} cost={cost} desc={desc} />
-            
+
         )
     }
-    else if(toggleOn == "deluxe"){
+    else if (toggleOn === "deluxe") {
         const title = "Deluxe Seating";
         const cost = "£12.50";
         const desc = "Deluxe seating provides you with the full QACinema experience, A new fully immersive feeling to movie viewing in premium seats with a premium view!";
-        return(
+        return (
             <ScreenInfoCard title={title} cost={cost} desc={desc} />
         )
     }
-    else{ 
+    else {
         return null;
-}
+    }
 }
 
 export default ReadMoreText
