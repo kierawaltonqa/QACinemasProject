41aff04d8617ae3d40e5beb90e40cf2e48ad52de
RobTolley <rtolley@qa.com>
2021-03-08 12:31:45 +0000

    basic tabbing for payment modal

---
+++ frontend/src/Components/TicketBooking/Sidebar/CardPayment.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const CardPayment = () => {
+    return (
+        <div className="form-mb position-relative">
+            <br/>
+            <label htmlFor="">Card Holder</label>
+            <div>
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default CardPayment
--- frontend/src/Components/TicketBooking/Sidebar/PaymentModal.jsx
+++ frontend/src/Components/TicketBooking/Sidebar/PaymentModal.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Tab, Tabs } from 'react-bootstrap'
 import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap'
+import CardPayment from './CardPayment';
 
 const PaymentModal = ({hidden}) => {
+
+    const [key, setKey] = useState('home');
+
+
     return (
         <Modal isOpen={hidden} className="modal-container modal-dialog modal-dialog-centered ">
         <ModalHeader style={{ backgroundColor: "blue", color: "white" }}>
@@ -10,9 +16,20 @@ const PaymentModal = ({hidden}) => {
         </ModalHeader>
         <ModalBody style={{ backgroundColor: "white" }} >
 
-            <div className="form-mb position-relative">
-               
-            </div>
+        <Tabs
+      id="controlled-tab-example"
+      activeKey={key}
+      onSelect={(k) => setKey(k)}
+    >
+      <Tab eventKey="home" title="Credit/Debit Cards">
+
+          <CardPayment/>
+        
+      </Tab>
+      <Tab eventKey="profile" title="PayPal">
+        
+      </Tab>
+    </Tabs>
 
 
         </ModalBody>
