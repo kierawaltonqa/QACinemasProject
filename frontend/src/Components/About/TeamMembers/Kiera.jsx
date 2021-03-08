e91ac22d6b5f1ba1ab358d50a57e27b69fc6da24
Kiera Walton <kwalton@qa.com>
2021-03-08 16:22:08 +0000

    edit personal message and added photo

--- frontend/src/Components/About/TeamMembers/Kiera.jsx
+++ frontend/src/Components/About/TeamMembers/Kiera.jsx
@@ -1,5 +1,6 @@
 import Media from "react-bootstrap/Media";
 import logo from "../../../Resources/Logo/logo.jpg"
+import kiera from "../../../Resources/kiera.JPG"
 
 const Kiera = () => {
 
@@ -15,15 +16,16 @@ const Kiera = () => {
                     padding={20}
                     height={64}
                     className="align-self-end mr-8"
-                    src={logo}
+                    src={kiera}
                     alt="Generic placeholder"
                 />
                 <h3>Kiera Walton</h3>
                 <p>
-                    Hi, I'm Kiera! I have loved collaborating with Team Spell to complete this project; using our individual strengths, we learned from and helped each other, working together to build this website and all the functionality it has to offer.
-                    For the most part, I worked on the front end development; designing and implementing the discussion board, the movie listings and new releases, as well as the search bar, and various other aspects of design and styling.
-                    I also took on the role of scrum master for this project, meaning I was in charge of running the daily stand ups.
+                    Hi, I'm Kiera! I have loved collaborating with Team Spell to complete this project.
+                    For the most part, I worked on the front end development; designing and implementing the discussion board, the movie listings and new releases, as well as the search bar, and various other aspects of the design and styling.
+                    I also took on the role of scrum master for this project, meaning I was in charge of running the daily stand ups and ensuring the successful completion of tasks and sprints.
         </p>
+
 
                 <p className="mb-0">
 
