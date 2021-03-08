5cfa59beb914745f2e58d3d7dede7761340486ff
Kiera Walton <kwalton@qa.com>
2021-03-08 12:32:07 +0000

    added details to bars modal

--- frontend/src/Components/About/PlacesToGo/TBModal.jsx
+++ frontend/src/Components/About/PlacesToGo/TBModal.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import turtlebay from "../../../Resources/turtle-bay-.jpg";
+import allbarone from "../../../Resources/allbar1.jpg";
 import Tabs from 'react-bootstrap/Tabs';
 import Tab from 'react-bootstrap/Tab';
 
@@ -23,18 +24,30 @@ const TBModal = () => {
                         activeKey={key}
                         onSelect={(k) => setKey(k)}
                     >
-                        <Tab eventKey="home" title="Home">
-                            <p>Turtle Bay</p>
+                        <Tab eventKey="home" title="Turtle Bay">
+                            <div className="row">
+                                <br />
+                                <center>
+                                    <img src={turtlebay} alt="turtlebay" height="100%" width="75%" />
+                                </center>
+                                <br />
+                                <p>Turtle Bay is a Carribean restaurant/bar that serves a selection of great food and cocktails.
+                                They have happy hour every day, offering 2 for 1 on drinks!
+                                    This is located a five minute walk away from QA Cinemas.</p>
+                            </div>
                         </Tab>
-                        <Tab eventKey="profile" title="Profile">
-                            <p>All Bar One</p>
+                        <Tab eventKey="profile" title="All Bar One">
+                            <div className="row">
+                                <br />
+                                <center>
+                                    <img src={allbarone} alt="allbarone" id="allbarone" height="75%" width="75%" />
+                                </center>
+                                <p>All Bar One is a stylish wine bar/pub that offers an extensive range of drinks at reasonable prices.
+                                    This is located just a few doors down from us and is the perfect place for a post-movie drink.</p>
+                            </div>
                         </Tab>
                     </Tabs>
-                    {/* <div className="row">
-                        <center>
-                            <img src={turtlebay} alt="turtlebay" height="75%" width="75%" />
-                        </center>
-                    </div> */}
+
                 </ModalBody>
                 <ModalFooter id="modalfooter">
                     <button onClick={toggle} className="btn btn-outline-danger">Go Back</button>
