aa93162a13323f423a98b4f9a9a8630e790b6c8e
RobTolley <rtolley@qa.com>
2021-03-09 17:17:04 +0000

    ticketbooking styling

--- frontend/src/Components/TicketBooking/MainPage/TicketBooking.jsx
+++ frontend/src/Components/TicketBooking/MainPage/TicketBooking.jsx
@@ -2,10 +2,12 @@ import React, { useState } from 'react'
 import ReadAll from '../FilmList/ReadAll'
 import Sidebar from '../Sidebar/Sidebar'
 import "../Resources/TicketBooking.css"
+import { Alert } from 'reactstrap'
 
 const TicketBooking = () => {
 
     const [basketid, setbasketid] = useState({});
+    const [ticketAlert, setticketAlert] = useState(false);
 
 
 
@@ -16,11 +18,12 @@ const TicketBooking = () => {
     return (
 
         <>
-        <hr />
+        <hr/>
             <div className="row">
                 <div className="col-9">
-                    
-                    <h2 style={{ color: "gold",  marginLeft: "25px" }}><center>Ticket Bookings:</center></h2>
+                    <br />
+                    <h2 style={{ color: "gold",  marginLeft: "25px" }}><center>Ticket Booking:</center></h2>
+                    <Alert isOpen={ticketAlert} style={{marginLeft:"25px", textAlign:"center"}} color="danger">Please select a movie and seats before purchasing!</Alert>
                     <br />
                     <br />
                     <div className="">
@@ -28,7 +31,7 @@ const TicketBooking = () => {
                     </div>
                 </div>
                 <div className="col-3" style={{backgroundColor:"black"}}>
-                    <Sidebar basketid={basketid}  />
+                    <Sidebar ticketalert={setticketAlert} basketid={basketid} style={{backgroundColor:"black"}} />
                 </div>
             </div>
         </>
