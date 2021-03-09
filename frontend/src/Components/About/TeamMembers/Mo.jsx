d9e030393053f1ab2d1729101d0c36ff0ad8dbae
Kiera Walton <kwalton@qa.com>
2021-03-09 12:53:39 +0000

    added Mo's message to about page

--- frontend/src/Components/About/TeamMembers/Mo.jsx
+++ frontend/src/Components/About/TeamMembers/Mo.jsx
@@ -12,11 +12,10 @@ const Mo = () => {
                 <div className="col-md-9">
                     <h3>Mohamud Mussa</h3>
                     <p>
-                        Cras sit amet nib h libero, in gravida nulla. Nulla vel metus
-                        scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum
-                        in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac
-                        nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                        Hi there! My name is Mo and the love I have for React is unmatched. I spend most days daydreaming about how the world is full of components and unidirectional communication.
                 </p>
+                    <p>Anywhoo, I was mainly involved in the frontend of this website, I did also design our custom images in Photoshop. Css is also a newfound joy of mine. Since we completed this website fairly quickly because our team is amazing,  I thought of the idea to create a blog page, setting up the backend, and linking it to a custom page that allows the admin staff to create blog posts!</p>
+                    <p>Here is my Github :)</p>
                 </div>
                 <div className="col-md-3">
                     <center>
