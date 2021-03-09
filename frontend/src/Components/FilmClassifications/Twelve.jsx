52689107da979127fec0ac5d037e1ff806e58a12
Kiera Walton <kwalton@qa.com>
2021-03-09 12:08:05 +0000

    added text to classification page

--- frontend/src/Components/FilmClassifications/Twelve.jsx
+++ frontend/src/Components/FilmClassifications/Twelve.jsx
@@ -5,35 +5,37 @@ import tw from "./Images/12.png"
 
 const Twelve = ({ toggleShow12 }) => {
 
-    return (
-        <>
-  <>
+  return (
+    <>
+      <>
 
-<div className="beauty"> 
-  <Toast.Header className="goldenHours">
-    <strong style={{color: 'red'}}>12 Film Ratings</strong>
-  </Toast.Header>
-  <Toast.Body  className="goldenHours">
+        <div className="beauty">
+          <Toast.Header className="goldenHours">
+            <strong style={{ color: 'red' }}>12 Film Ratings</strong>
+          </Toast.Header>
+          <Toast.Body className="goldenHours">
 
-  <Image style={{backgroundColor: 'gold', height: '30%', width: '30%'}} src={tw} rounded />
-  <hr/>
+            <Image style={{ backgroundColor: 'gold', height: '30%', width: '30%' }} src={tw} rounded />
+            <hr />
 
-      <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis ipsa voluptas aliquam architecto est ea ab molestias molestiae voluptate necessitatibus dicta reprehenderit aut quos laboriosam veritatis, eius optio accusantium expedita! </p>
-      <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis ipsa voluptas aliquam architecto est ea ab molestias molestiae voluptate necessitatibus dicta reprehenderit aut quos laboriosam veritatis, eius optio accusantium expedita! </p>
-      <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis ipsa voluptas aliquam architecto est ea ab molestias molestiae voluptate necessitatibus dicta reprehenderit aut quos laboriosam veritatis, eius optio accusantium expedita! </p>
-      <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis ipsa voluptas aliquam architecto est ea ab molestias molestiae voluptate necessitatibus dicta reprehenderit aut quos laboriosam veritatis, eius optio accusantium expedita! </p>
-    <a style={{color: 'red'}}  
-      href="https://www.bbfc.co.uk/rating/12"
-      rel="noreferrer"
-      target="_blank"
-    >
-      Additional Info
+            <strong style={{ color: 'red' }}>12 – Video release suitable for 12 years and over</strong>
+            <p style={{ color: 'red' }}>video works classified 12 contain material that is not generally suitable for children aged under 12. No one younger than 12 may see a 12A film in a cinema unless accompanied by an adult. Adults planning to take a child under 12 to view a 12A film should consider whether the film is suitable for that child. To help them decide, we recommend that they check the Ratings Info for that film in advance. No one younger than 12 may rent or buy a 12 rated video work.</p>
+            <strong>Dangerous Behaviour</strong>
+            <p>No promotion of potentially dangerous behaviour which children are likely to copy. No glamorisation of realistic or easily accessible weapons such as knives. No endorsement of anti-social behaviour.</p>
+            <strong>Discrimination</strong>
+            <p>Discriminatory language or behaviour must not be endorsed by the work as a whole. Aggressive discriminatory language or behaviour is unlikely to be acceptable unless clearly condemned.</p>
+            <a style={{ color: 'red' }}
+              href="https://www.bbfc.co.uk/rating/12"
+              rel="noreferrer"
+              target="_blank"
+            >
+              Additional Info
     </a>
-  </Toast.Body>
-  </div>
-</>
-        </>
-    )
+          </Toast.Body>
+        </div>
+      </>
+    </>
+  )
 
 }
 
