9ff07af36a2c66db48c2ea51709748f3cc678979
Mohamud Mussa <70893158+MohamudMussa@users.noreply.github.com>
2021-03-12 16:46:51 +0000

    Update EachBlog.jsx

--- frontend/src/Components/Blog/EachBlog.jsx
+++ frontend/src/Components/Blog/EachBlog.jsx
@@ -2,7 +2,6 @@ import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 import React, { useState } from 'react';
 
-import image from '../../Resources/Logo/lost.jpg'
 
 
 const EachBlog = ({ item }) => {
@@ -19,13 +18,7 @@ const EachBlog = ({ item }) => {
         
 
         <div style={{color: 'gold'}}>
-            <img
-    width={64}
-    height={64}
-    className="mr-3"
-    src={image}
-    alt="Generic placeholder"
-  />
+          
         <h1> {item.title} </h1>
         <p> {item.lilInfo} </p>
           <Button color="warning" onClick={toggle}> <strong>Read More</strong> </Button>
